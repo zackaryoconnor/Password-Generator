091ea5ac9817c903eb34993fe6821d020f69ef5d
Zack OConnor <zackary25@gmail.com>
2025-09-19 13:35:27 -0400

    User can now select if they want capital, numbers, and symbols in password.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,97 @@
-import {useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function App() {
-  const capitalCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-  const lowerCharacters = 'abcdefghijklmnopqrstuvwxyz'
-  const numbers = '0123456789'
-  const symbols = '!@#$%^&*()<>?'
-
   const [password, setPassword] = useState('Random Password.')
+  const [passwordLength, setPasswordLength] = useState(16)
+  const [includeCapitals, setIncludeCapitals] = useState(true)
+  const [includeNumbers, setIncludeNumbers] = useState(true)
+  const [includeSymbols, setIncludeSymbols] = useState(true)
 
-  const getRandomCharacters = (source: string, length: number) => {
-    let result = ''
-    for (let index = 0; index < length; index++) {
-      result += source.charAt(Math.floor(Math.random() * source.length))
+  const getRandomCharacters = () => {
+    const lowerCharacters = 'abcdefghijklmnopqrstuvwxyz'
+    const capitalCharacters = lowerCharacters.toUpperCase()
+    const numbers = '0123456789'
+    const symbols = '!@#$%^&*()<>?'
+
+    const characters: string =
+      lowerCharacters +
+      (includeCapitals ? capitalCharacters : '') +
+      (includeNumbers ? numbers : '') +
+      (includeSymbols ? symbols : '')
+
+    let password: string = ''
+
+    for (let index = 0; index < passwordLength; index++) {
+      password += characters[Math.floor(Math.random() * characters.length)]
     }
-    return result
+    setPassword(password)
   }
+
+  const handleRangeSlider = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const updatedLength = Number(event.target.value)
+    setPasswordLength(updatedLength)
+  }
+
+  const copyToClipboard = (textToCopy: string) => {
+    navigator.clipboard.writeText(textToCopy)
+  }
+
+  useEffect(() => {
+    getRandomCharacters()
+  }, [passwordLength, includeCapitals, includeNumbers, includeSymbols])
 
   return (
     <div>
       <h1>{password}</h1>
-      <button onClick={() => setPassword(getRandomCharacters(capitalCharacters + lowerCharacters + numbers + symbols, 16))}>Generate Password</button>
+      <button onClick={() => copyToClipboard(password)}>copy password</button>
+      <label>
+        Password Length:
+        <input
+          type="range"
+          min={8}
+          max={32}
+          value={passwordLength}
+          onChange={handleRangeSlider}
+        />
+        <span>{passwordLength}</span>
+      </label>
+      <button onClick={getRandomCharacters}>Generate Password</button>
+
+      <form>
+        <div>
+          <label htmlFor="capital">
+            Capital Letters
+            <input
+              type="checkbox"
+              id="capital"
+              checked={includeCapitals}
+              onChange={() => setIncludeCapitals(!includeCapitals)}
+            />
+          </label>
+        </div>
+        <div>
+          <label htmlFor="numbers">
+            Numbers
+            <input
+              type="checkbox"
+              id="numbers"
+              checked={includeNumbers}
+              onChange={() => setIncludeNumbers(!includeNumbers)}
+            />
+          </label>
+        </div>
+        <div>
+          <label htmlFor="symbols">
+            Symbols
+            <input
+              type="checkbox"
+              id="symbols"
+              checked={includeSymbols}
+              onChange={() => setIncludeSymbols(!includeSymbols)}
+            />
+          </label>
+        </div>
+      </form>
     </div>
   )
 }
